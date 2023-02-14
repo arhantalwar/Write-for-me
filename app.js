4eb2350684600b47da5a018d2c81f0c6b6e27025
arhantalwar <arhantalwar@gmail.com>
2023-02-14 20:57:29 +0530

    Added

---
+++ app.js
@@ -0,0 +1,405 @@
+var Container = document.getElementById("Container");
+
+var para = document.getElementById("para");
+const para_text = para.innerText;
+const para_array = para_text.split('');
+
+for(let i = 0; i < para_array.length; i++){
+  x = para_array[i];
+  if(x == "a" || x == "A"){
+    
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','a');
+    Child_container.src = "Resources/Wingman/a.png";
+    Container.append(Child_container);
+
+  } else if (x == "b" || x == "B") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','b');
+    Child_container.src = "Resources/Wingman/b.png";
+    Container.append(Child_container);
+
+  } else if (x == "c" || x == "C") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','c');
+    Child_container.src = "Resources/Wingman/c.png";
+    Container.append(Child_container);
+
+  } else if (x == "d" || x == "D") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','d');
+    Child_container.src = "Resources/Wingman/d.png";
+    Container.append(Child_container);
+
+  } else if (x == "e" || x == "E") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','e');
+    Child_container.src = "Resources/Wingman/e.png";
+    Container.append(Child_container);
+
+  } else if (x == "f" || x == "F") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','f');
+    Child_container.src = "Resources/Wingman/f.png";
+    Container.append(Child_container);
+
+  } else if (x == "g" || x == "G") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','g');
+    Child_container.src = "Resources/Wingman/g.png";
+    Container.append(Child_container);
+
+  } else if (x == "h" || x == "H") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','h');
+    Child_container.src = "Resources/Wingman/h.png";
+    Container.append(Child_container);
+
+  } else if (x == "i" || x == "I") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','i');
+    Child_container.src = "Resources/Wingman/i.png";
+    Container.append(Child_container);
+
+  } else if (x == "j" || x == "J") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','j');
+    Child_container.src = "Resources/Wingman/j.png";
+    Container.append(Child_container);
+
+  } else if (x == "k" || x == "K") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','k');
+    Child_container.src = "Resources/Wingman/k.png";
+    Container.append(Child_container);
+
+  } else if (x == "l" || x == "L") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','l');
+    Child_container.src = "Resources/Wingman/l.png";
+    Container.append(Child_container);
+
+  } else if (x == "m" || x == "M") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','m');
+    Child_container.src = "Resources/Wingman/m.png";
+    Container.append(Child_container);
+
+  } else if (x == "n" || x == "N") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','n');
+    Child_container.src = "Resources/Wingman/n.png";
+    Container.append(Child_container);
+
+  } else if (x == "o" || x == "O") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','o');
+    Child_container.src = "Resources/Wingman/o.png";
+    Container.append(Child_container);
+
+  } else if (x == "p" || x == "P") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','p');
+    Child_container.src = "Resources/Wingman/p.png";
+    Container.append(Child_container);
+
+  } else if (x == "q" || x == "Q") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','q');
+    Child_container.src = "Resources/Wingman/q.png";
+    Container.append(Child_container);
+
+  } else if (x == "r" || x == "R") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','r');
+    Child_container.src = "Resources/Wingman/r.png";
+    Container.append(Child_container);
+
+  } else if (x == "s" || x == "S") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','s');
+    Child_container.src = "Resources/Wingman/s.png";
+    Container.append(Child_container);
+
+  } else if (x == "t" || x == "T") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','t');
+    Child_container.src = "Resources/Wingman/t.png";
+    Container.append(Child_container);
+
+  } else if (x == "u" || x == "U") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','u');
+    Child_container.src = "Resources/Wingman/u.png";
+    Container.append(Child_container);
+
+  } else if (x == "v" || x == "V") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','v');
+    Child_container.src = "Resources/Wingman/v.png";
+    Container.append(Child_container);
+
+  } else if (x == "w" || x == "W") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','w');
+    Child_container.src = "Resources/Wingman/w.png";
+    Container.append(Child_container);
+
+  } else if (x == "x" || x == "X") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','x');
+    Child_container.src = "Resources/Wingman/x.png";
+    Container.append(Child_container);
+
+  } else if (x == "y" || x == "Y") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','y');
+    Child_container.src = "Resources/Wingman/y.png";
+    Container.append(Child_container);
+
+  } else if (x == "z" || x == "Z") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','z');
+    Child_container.src = "Resources/Wingman/z.png";
+    Container.append(Child_container);
+
+  } else if (x == "1") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','1');
+    Child_container.src = "Resources/Wingman/1.png";
+    Container.append(Child_container);
+
+  } else if (x == "2") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','2');
+    Child_container.src = "Resources/Wingman/2.png";
+    Container.append(Child_container);
+
+  } else if (x == "3") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','3');
+    Child_container.src = "Resources/Wingman/3.png";
+    Container.append(Child_container);
+
+  } else if (x == "4") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','4');
+    Child_container.src = "Resources/Wingman/4.png";
+    Container.append(Child_container);
+
+  } else if (x == "5") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','5');
+    Child_container.src = "Resources/Wingman/5.png";
+    Container.append(Child_container);
+
+  } else if (x == "6") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','6');
+    Child_container.src = "Resources/Wingman/6.png";
+    Container.append(Child_container);
+
+  } else if (x == "7") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','7');
+    Child_container.src = "Resources/Wingman/7.png";
+    Container.append(Child_container);
+
+  } else if (x == "8") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','8');
+    Child_container.src = "Resources/Wingman/8.png";
+    Container.append(Child_container);
+
+  } else if (x == "9") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','9');
+    Child_container.src = "Resources/Wingman/9.png";
+    Container.append(Child_container);
+
+  } else if (x == "0") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','0');
+    Child_container.src = "Resources/Wingman/0.png";
+    Container.append(Child_container);
+
+  } else if (x == "<") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','left_arrow');
+    Child_container.src = "Resources/Wingman/left_arrow.png";
+    Container.append(Child_container);
+
+  } else if (x == ">") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','right_arrow');
+    Child_container.src = "Resources/Wingman/right_arrow.png";
+    Container.append(Child_container);
+
+  } else if (x == "{") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','left_curley');
+    Child_container.src = "Resources/Wingman/left_curley.png";
+    Container.append(Child_container);
+
+  } else if (x == "}") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','right_curley');
+    Child_container.src = "Resources/Wingman/right_curley.png";
+    Container.append(Child_container);
+
+  } else if (x == "(") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','left_pranthesses');
+    Child_container.src = "Resources/Wingman/left_pranthesses.png";
+    Container.append(Child_container);
+
+  } else if (x == ")") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','right_pranthesses');
+    Child_container.src = "Resources/Wingman/right_pranthesses.png";
+    Container.append(Child_container);
+
+  } else if (x == "[") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','left_square');
+    Child_container.src = "Resources/Wingman/left_square.png";
+    Container.append(Child_container);
+
+  } else if (x == "]") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','right_square');
+    Child_container.src = "Resources/Wingman/right_square.png";
+    Container.append(Child_container);
+
+  } else if (x == "-") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','hypen');
+    Child_container.src = "Resources/Wingman/hypen.png";
+    Container.append(Child_container);
+
+  } else if (x == ".") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','full_stop');
+    Child_container.src = "Resources/Wingman/full_stop.png";
+    Container.append(Child_container);
+
+  } else if (x == ";") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','semi_colon');
+    Child_container.src = "Resources/Wingman/semi_colon.png";
+    Container.append(Child_container);
+
+  } else if (x == ":") {
+
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.setAttribute('class','colon');
+    Child_container.src = "Resources/Wingman/colon.png";
+    Container.append(Child_container);
+
+  } else if (x == ",") {
+
+    continue;
+
+  } else {
+    
+    var Child_container = document.createElement("img");
+    Child_container.setAttribute('id','child');
+    Child_container.src = "Resources/Wingman/blank_space.png";
+    Container.append(Child_container);
+
+  }
+}
